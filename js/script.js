49696e52f6896e0b333bb0d2d5df013be3e44d6f
NINJA68OKLM <85460270+NINJA68OKLM@users.noreply.github.com>
2022-01-02 17:34:16 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-// Script pur le mode sombre :
+// var body = document.querySelector("body");
+// if (body.style.width>="200px")
+// {
+//     document.querySelector(".flex1.flex2>.flexcenter>img").src= "img/Logo dark.png";
+// }
+
+// Script pour le mode sombre :
 var a = document.querySelectorAll("a");
 document.querySelector(".a.invisible>.flex3.flexcenter").style.stroke="black";
-// Dérouleent du menu portablme et tablette :
+// Déroulement du menu portable et tablette :
 document.querySelector(".absolute").addEventListener("click", deroul);
 function deroul(){
     document.querySelector(".a").classList.toggle("invisible");
@@ -12,16 +18,16 @@ function sombree(){
     document.querySelector(".a>.flex3.flexcenter").children[0].classList.add("invisible");
     document.querySelector(".a>.flex3.flexcenter").children[1].classList.remove("invisible");
     document.querySelector(".a>.flex3.flexcenter").children[1].style.display= "initial";
-    document.querySelector(".a>.flex3.flexcenter").children[0].style.stroke= "black";
+    document.querySelector(".a>.flex3.flexcenter").children[0].style.stroke= "white";
     document.querySelector("body").classList.add("sombre");
     document.querySelector("header").classList.add("sombre2");
     a.forEach(e => {
-        e.style.color= "black";
+        e.style.color= "white";
     });
     document.querySelector(".a").children[0].classList.add("invisible");
-    document.querySelector(".a").children[7].style.stroke="black";
-    document.querySelector(".absolute").style.stroke="black";
-    document.querySelector(".flex1.flex2>.flexcenter>img").src= "img/Logo dark.png";
+    document.querySelector(".a").children[7].style.stroke="white";
+    document.querySelector(".absolute").style.stroke="white";
+    document.querySelector(".flex1.flex2>.flexcenter>img").src= "img/Logo light.png";
 }
 // Nuit PC :
 document.querySelector(".a").children[0].addEventListener("click", sombre);
@@ -29,13 +35,13 @@ function sombre(){
     document.querySelector("body").classList.add("sombre");
     document.querySelector("header").classList.add("sombre2");
     a.forEach(e => {
-        e.style.color= "black";
+        e.style.color= "white";
     });
     document.querySelector(".a").children[1].classList.remove("invisible");
     document.querySelector(".a").children[0].classList.add("invisible");
-    document.querySelector(".a").children[7].style.stroke="black";
+    document.querySelector(".a").children[7].style.stroke="white";
     document.querySelector(".a").children[1].style.display= "initial";
-    document.querySelector(".flex1.flex2>.flexcenter>img").src= "img/Logo dark pc.png";
+    document.querySelector(".flex1.flex2>.flexcenter>img").src= "img/Logo light pc.png";
     document.querySelector(".fixed").style.border= "3px solid white";
     document.querySelector(".fixed").style.background= "black";
     document.querySelector(".fixed>a>svg").style.stroke= "white";
@@ -46,13 +52,13 @@ function clair(){
     document.querySelector("body").classList.remove("sombre");
     document.querySelector("header").classList.remove("sombre2");
     a.forEach(e => {
-        e.style.color= "white";
+        e.style.color= "black";
     });
     document.querySelector(".a").children[0].classList.remove("invisible");
     document.querySelector(".a").children[1].classList.add("invisible");
     document.querySelector(".a").children[1].style.display= "none";
-    document.querySelector(".a").children[7].style.stroke="white";
-    document.querySelector(".flex1.flex2>.flexcenter>img").src= "img/Logo light pc.png";
+    document.querySelector(".a").children[7].style.stroke="black";
+    document.querySelector(".flex1.flex2>.flexcenter>img").src= "img/Logo dark pc.png";
     document.querySelector(".fixed").style.border= "3px solid black";
     document.querySelector(".fixed").style.background= "white";
     document.querySelector(".fixed>a>svg").style.stroke= "black";
@@ -63,7 +69,7 @@ function claire(){
     document.querySelector(".a>.flex3.flexcenter").children[0].classList.remove("invisible");
     document.querySelector(".a>.flex3.flexcenter").children[1].classList.add("invisible");
     document.querySelector(".a>.flex3.flexcenter").children[1].style.display= "none";
-    document.querySelector(".a>.flex3.flexcenter").children[0].style.stroke= "white";
+    document.querySelector(".a>.flex3.flexcenter").children[0].style.stroke= "black";
     document.querySelector("body").classList.remove("sombre");
     document.querySelector("header").classList.remove("sombre2");
     a.forEach(e => {
@@ -71,9 +77,9 @@ function claire(){
     });
     document.querySelector(".a").children[0].classList.remove("invisible");
     document.querySelector(".a").children[1].classList.add("invisible");
-    document.querySelector(".a").children[7].style.stroke="white";
-    document.querySelector(".absolute").style.stroke="white";
-    document.querySelector(".flex1.flex2>.flexcenter>img").src= "img/Logo light.png";
+    document.querySelector(".a").children[7].style.stroke="black";
+    document.querySelector(".absolute").style.stroke="black";
+    document.querySelector(".flex1.flex2>.flexcenter>img").src= "img/Logo dark.png";
 }
 
 // Script pour la barre de recherche :
@@ -99,10 +105,19 @@ photo.forEach(e => {
 
 // Script pour le scroll :
 
-var scroll = document.querySelector(".fixed");
+var scroll = document.querySelectorAll(".fixed, .fixed>a, .fixed>a>svg");
 var fleche = document.querySelector(".fixed>a>svg");
-if (scroll.style.position= "fixed")
-{
-    scroll.style.display= "initial";
-    fleche.style.display= "initial";
+document.querySelector("#qui-sommes-nous").addEventListener("mouseenter", scroll);
+function scroll(){
+    scroll.forEach(e => {
+        if (document.querySelector(".fixed").style.width= "40px")
+        {
+            e.style.display= "initial";
+        }
+    });
 }
+
+// Script pour le bas de page :
+
+document.querySelector(".flexcenter.bds").style.background= "black";
+document.querySelector(".flexcenter.bds").style.color= "white";
